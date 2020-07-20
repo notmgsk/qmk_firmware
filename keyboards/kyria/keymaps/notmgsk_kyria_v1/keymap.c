25117703f6be664862df9c4074d6d3c7ba82eaea
Mark Skilbeck <mark.skilbeck@rigetti.com>
2020-07-19 20:36:59 -0700

    keys

--- keyboards/kyria/keymaps/notmgsk_kyria_v1/keymap.c
+++ keyboards/kyria/keymaps/notmgsk_kyria_v1/keymap.c
@@ -2,26 +2,26 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_PIPE,
-    KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(2,KC_SCLN), KC_QUOT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LCTL_T(KC_SPACE), LT(3,KC_ESC), KC_SPC, LT(1,KC_ENT), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_ESC, KC_LALT, KC_LGUI, LCTL_T(KC_SPACE), KC_TRNS, KC_TRNS, LT(1,KC_ENT), KC_RGUI, KC_BSPC, KC_RALT),
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_PIPE,
+        KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(2,KC_SCLN), KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LCTL_T(KC_SPACE), LT(3,KC_ESC), KC_SPC, LT(1,KC_ENT), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_ESC, KC_LALT, KC_LGUI, LCTL_T(KC_SPACE), LT(3,KC_ESC), KC_SPC, LT(1,KC_ENT), KC_RGUI, KC_BSPC, KC_RALT),
 
 
     [1] = LAYOUT(
-    KC_TRNS, KC_P0, KC_P1, KC_P2, KC_P3, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
-    KC_TRNS, KC_HASH, KC_P4, KC_P5, KC_P6, KC_GRV, KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
-    KC_TRNS, KC_PERC, KC_P7, KC_P8, KC_P9, KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AMPR, KC_EQL,
-    KC_COMM, KC_DOT, KC_SLSH, KC_MINS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SCLN, KC_EQL, KC_EQL, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_P0, KC_P1, KC_P2, KC_P3, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
+        KC_TRNS, KC_HASH, KC_P4, KC_P5, KC_P6, KC_GRV, KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
+        KC_TRNS, KC_PERC, KC_P7, KC_P8, KC_P9, KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_AMPR, KC_EQL,
+        KC_COMM, KC_DOT, KC_SLSH, KC_MINS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SCLN, KC_EQL, KC_EQL, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS),
 
 
     [2] = LAYOUT(
-    KC_TRNS, KC_CIRC, KC_PAST, KC_PPLS, KC_PEQL, KC_AMPR, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_TRNS, KC_TRNS,
-    KC_EQL, KC_TILD, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT, KC_GT, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_PERC, KC_GRV, KC_QUOT, KC_SCLN, KC_QUES, KC_TRNS, KC_TRNS, KC_UNDS, KC_DQUO, KC_MINS, KC_COLN,
-    KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TRNS, KC_CIRC, KC_PAST, KC_PPLS, KC_PEQL, KC_AMPR, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_TRNS, KC_TRNS,
+        KC_EQL, KC_TILD, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT, KC_GT, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_PERC, KC_GRV, KC_QUOT, KC_SCLN, KC_QUES, KC_TRNS, KC_TRNS, KC_UNDS, KC_DQUO, KC_MINS, KC_COLN,
+        KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-  // Lifted from Thomas' config.
+// Lifted from Thomas' config.
 /*
  * 3 Layer: Numpad, Media
  *
@@ -36,12 +36,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      | Lower|      |      |  |      | Nav  | Raise|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-  [3] = LAYOUT(
-    _______, _______, _______, KC_VOLU, _______, _______,                                     KC_SLSH, KC_7,    KC_8,    KC_9, KC_MINS, _______,
-    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                                     KC_ASTR, KC_4,    KC_5,    KC_6, KC_COMM, KC_PLUS,
-    _______, _______, _______, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3, KC_EQL,  _______,
-                               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
+    [3] = LAYOUT(
+        _______, _______, _______, KC_VOLU, _______, _______,                                     KC_SLSH, KC_7,    KC_8,    KC_9, KC_MINS, _______,
+        _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                                     KC_ASTR, KC_4,    KC_5,    KC_6, KC_COMM, KC_PLUS,
+        _______, _______, _______, KC_VOLD, KC_MUTE, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3, KC_EQL,  _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        ),
 };
 
 #ifdef OLED_DRIVER_ENABLE
