31838bc5e410e1eb6d9c277d489defeae4eeaa53
Mark Skilbeck <mark.skilbeck@rigetti.com>
2020-07-20 20:00:04 -0700

    Use OSL instead of LT, copy ergodox config
    OSL is more receptive to a fast simultaneous keypress. LT often
    registered as two keytaps, rather than a LT + a keypress.

--- keyboards/kyria/keymaps/notmgsk_kyria_v1/config.h
+++ keyboards/kyria/keymaps/notmgsk_kyria_v1/config.h
@@ -5,18 +5,12 @@
 
 #pragma once
 
-#define TAPPING_TOGGLE  1
-
-#define TAPPING_TERM    150
-// this makes it possible to do rolling combos (zx) with keys that convert to
-// other keys on hold (z becomes ctrl when you hold it, and when this option
-// isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+#define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 
 #undef DEBOUNCE
 #define DEBOUNCE 40
-
-// The following is lifted from the thomasbaart config
 
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 200
@@ -32,20 +26,4 @@
   #define RGBLIGHT_VAL_STEP 8
 #endif
 
-// EC11K encoders have a different resolution than other EC11 encoders.
-// When using the default resolution of 4, if you notice your encoder skipping
-// every other tick, lower the resolution to 2.
-// #define ENCODER_RESOLUTION 2
 
-// The Leader key allows to flexibly assign macros to key sequences.
-// #define LEADER_PER_KEY_TIMING
-// #define LEADER_TIMEOUT 350
-
-// #define TAPPING_TERM 200
-
-// Allows to use either side as the master. Look at the documentation for info:
-// https://docs.qmk.fm/#/config_options?id=setting-handedness
-// #define EE_HANDS
-
-// Allows media codes to properly register in macros and rotary encoder code
-// #define TAP_CODE_DELAY 10
